f1a0c4fe47de8c1effd1e55cc76eea49988494f3
DogukanTaskiran <105669729+DogukanTaskiran@users.noreply.github.com>
2022-05-21 13:33:03 +0300

    Add files via upload

---
+++ Sword.java
@@ -0,0 +1,31 @@
+public class Sword extends Weapon {
+
+    private double parry;
+
+    public void setParry(double parry) {
+
+        this.parry = parry;
+    }
+
+    public double getParry() {
+
+        return parry;
+    }
+
+    public Sword(String name, double value, double weight, double damage, double parry) {
+        super.setName(name);
+        super.setValue(value);
+        super.setWeight(weight);
+        super.setDamage(damage * 2); // 2 yerine Strength gelecek.
+        this.setParry(parry);
+    }
+
+    @Override
+    public void ItemInfo() {
+        System.out.println("\nName: " + getName());
+        System.out.println("Weight: " + getWeight());
+        System.out.println("Value: " + getValue());
+        System.out.println("Damage: " + getDamage());
+        System.out.println("Parry: " + getParry());
+    }
+}
