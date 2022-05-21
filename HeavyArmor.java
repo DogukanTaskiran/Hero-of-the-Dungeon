739a55d82da4611e0de3585c82554cab19240799
elifilkay <93220459+elifilkay@users.noreply.github.com>
2022-05-21 14:14:05 +0300

    Add files via upload

---
+++ HeavyArmor.java
@@ -0,0 +1,11 @@
+public class HeavyArmor extends Armor {
+    public HeavyArmor(String name,double weight,double value,double resistance){
+        super(name, weight, value, resistance);
+
+    }
+
+    @Override
+    public void ItemInfo() {
+        super.ItemInfo();
+    }
+}
