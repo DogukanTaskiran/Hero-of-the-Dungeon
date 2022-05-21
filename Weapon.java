b57be40b5f1843eb5ca34559edfe30233d3dc344
elifilkay <93220459+elifilkay@users.noreply.github.com>
2022-05-21 15:39:19 +0300

    Add files via upload

--- Weapon.java
+++ Weapon.java
@@ -1,12 +1,25 @@
-public abstract class Weapon extends Items{
-    private double damage;
 
-    public void setDamage(double damage) {
+    public abstract class Weapon extends Items{
+        private double damage;
+        public Weapon(String name,double weight,double value,double damage){
+            super(name, weight, value);
+            this.damage=damage;
 
-        this.damage = damage;
+        }
+
+        public void setDamage(double damage) {
+
+            this.damage = damage;
+        }
+        public double getDamage() {
+
+            return damage;
+        }
+
+        @Override
+        public void ItemInfo() {
+            super.ItemInfo();
+            System.out.println("Damage: "+getDamage());
+        }
     }
-    public double getDamage() {
 
-        return damage;
-    }
-}
